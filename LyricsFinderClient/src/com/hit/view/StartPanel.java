97c3213c9d1c682a28bf5253787e52f146c8e787
danprophet <76520987+danprophet@users.noreply.github.com>
2022-06-21 00:37:16 +0300

    First GUI running

--- LyricsFinderClient/src/com/hit/view/StartPanel.java
+++ LyricsFinderClient/src/com/hit/view/StartPanel.java
@@ -1,19 +1,41 @@
 package com.hit.view;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class StartPanel extends JPanel implements ActionListener{
 	
 	public StartPanel()
 	{
 		super (new BorderLayout());
+		
+        JButton searchMode = new JButton("Search Mode");
+        searchMode.setSelected(true);
+        
+        JButton manageSongDB = new JButton("Manage Songs DB");
+        manageSongDB.setSelected(true);
+        
+        ButtonGroup group = new ButtonGroup();
+        group.add(searchMode);
+        group.add(manageSongDB);
+        
+        JPanel buttonPanel = new JPanel(new GridLayout(0, 1));
+        buttonPanel.add(searchMode);
+        buttonPanel.add(manageSongDB);
+        
+        
+        add(buttonPanel);
 	}
 	
 	private static void createAndShowGUI()
